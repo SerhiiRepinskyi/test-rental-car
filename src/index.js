1b9bb6d40a714d620ade5ce1931d53ca2e6425b4
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-09-16 19:46:40 +0300

    feat: added saving favoriteCars in LocalStorage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import "./index.css";
 import App from "./components/App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/test-rental-car">
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/test-rental-car">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { carsReducer } from "./carsSlice";
-// import { filterReducer } from "./filterSlice";
+// import { filterReducer } from "./contacts/filterSlice";
+
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+// Ігнорує ряд екшенів для виправлення помилок серіалізації при записі в localStorage
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+// Persisting token field from auth slise to localstorage
+const carsPersistConfig = {
+  key: "cars", // Назва ключа в LocalStorage
+  storage,
+  whitelist: ["favoriteCars"],
+};
 
 export const store = configureStore({
   reducer: {
-    cars: carsReducer,
+    cars: persistReducer(carsPersistConfig, carsReducer),
     // filter: filterReducer,
   },
+  middleware,
 });
+
+export const persistor = persistStore(store);
