047c71dde8df3dd24e30ecd75fa156f20cabe07f
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-09-17 23:12:07 +0300

    feat: added fixed header

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Header = styled.header`
   box-shadow: 0px 25px 25px -30px rgba(32, 32, 135, 0.7);
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: white;
+  z-index: 999;
 `;
 
 export const ContainerHeader = styled.div`
